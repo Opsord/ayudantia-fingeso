0ac66abc4bb004f4cd6d1f9a9227d18b932fb52b
Andres Zelaya <andres.zelaya.d@hotmail.com>
2024-12-04 09:18:19 -0300

    Inicio de proyecto

---
+++ supercar-webapp/src/main/java/grupo_b/supercar_webapp/SupercarWebappApplication.java
@@ -0,0 +1,13 @@
+package grupo_b.supercar_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercarWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercarWebappApplication.class, args);
+	}
+
+}
---
+++ supercar-webapp/src/test/java/grupo_b/supercar_webapp/SupercarWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo_b.supercar_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercarWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
